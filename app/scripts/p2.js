d5157c10be03ceca0430ba93d0d6c3e75c7dd678
Your Name <you@example.com>
2017-04-22 23:40:39 -0400

    Almost final ver

--- app/scripts/p2.js
+++ app/scripts/p2.js
@@ -1,15 +1,14 @@
 $(document).ready(function() { 
 
+
+
+
   $('.slider').slider();
 
+  $('#pagepiling').pagepiling({
+  sectionsColor: ['#2ecc71', '#f39c12', '#2ecc71']
 
-$('#fullpage').fullpage({
-	sectionsColor: ['#f39c12', '#2ecc71', 'purple'],
-  verticalCentered: false,
-  scrollOverflow: true
 });
-
-
 
 
 
@@ -17,15 +16,44 @@ $('#fullpage').fullpage({
 $('#demo01').animatedModal({
 animatedIn: 'lightSpeedIn',
 animatedOut: 'bounceOut',
-color:'#3498db'
+color:'#27ae60'
 
 });
 
-
+/*
 $('#demo02').animatedModal({
 animatedIn: 'lightSpeedIn',
 animatedOut: 'bounceOut',
 color:'#3498db'
 
 });
+*/
+$('#start').hide();
+$('#intext').hide();
+//Intro animations 
 
+setTimeout( function(){ 
+    // Do something after 3 second 
+      $('.menu').toggleClass('menu-active');
+
+  }  , 3000 );
+
+
+setTimeout( function(){ 
+    // Do something after 3.5 second 
+      $('#feather').hide();
+
+  }  , 3500 );
+
+
+setTimeout( function(){ 
+    // Do something after 4 second 
+      $('#start').addClass('animated zoomIn').show();
+
+  }  , 4000 );
+
+setTimeout( function(){ 
+    // Do something after 4 second 
+      $('#intext').addClass('animated fadeIn').show();
+
+  }  , 4200 );
