d7c19ebc94cf8065746795b03285bb9dfc5372d3
Your Name <you@example.com>
2017-05-01 01:47:55 -0400

    Added the rest of the content. Various fixes and style changes

--- app/scripts/p2.js
+++ app/scripts/p2.js
@@ -13,21 +13,8 @@ $(document).ready(function() {
 
 
 });
-$('#demo01').animatedModal({
-animatedIn: 'lightSpeedIn',
-animatedOut: 'bounceOut',
-color:'#27ae60'
 
-});
 
-/*
-$('#demo02').animatedModal({
-animatedIn: 'lightSpeedIn',
-animatedOut: 'bounceOut',
-color:'#3498db'
-
-});
-*/
 $('#start').hide();
 $('#intext').hide();
 //Intro animations 
@@ -57,3 +44,136 @@ setTimeout( function(){
       $('#intext').addClass('animated fadeIn').show();
 
   }  , 4200 );
+
+var x1 = document.getElementById('myAudio'); 
+var x2 = document.getElementById('myAudio2'); 
+var x3 = document.getElementById('myAudio3'); 
+var x4 = document.getElementById('myAudio4'); 
+var x5 = document.getElementById('myAudio5'); 
+var x6 = document.getElementById('myAudio6'); 
+var x7 = document.getElementById('myAudio7'); 
+var x8 = document.getElementById('myAudio8'); 
+var x9 = document.getElementById('myAudio9'); 
+var x10 = document.getElementById('myAudio10'); 
+
+function playAudio() { 
+    x1.play(); 
+} 
+
+function playAudio2() { 
+    x2.play(); 
+} 
+
+function playAudio3() { 
+    x3.play(); 
+} 
+
+function playAudio4() { 
+    x4.play(); 
+} 
+
+function playAudio5() { 
+    x5.play(); 
+} 
+
+function playAudio6() { 
+    x6.play(); 
+} 
+
+function playAudio7() { 
+    x7.play(); 
+} 
+
+function playAudio8() { 
+    x8.play(); 
+} 
+
+function playAudio9() { 
+    x9.play(); 
+} 
+
+function playAudio10() { 
+    x10.play(); 
+} 
+
+
+$('#demo01').animatedModal({
+  animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+
+});
+
+//demo 02
+$('#demo02').animatedModal({
+modalTarget:'modal-02',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+
+$('#demo03').animatedModal({
+modalTarget:'modal-03',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+$('#demo04').animatedModal({
+modalTarget:'modal-04',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+$('#demo05').animatedModal({
+modalTarget:'modal-05',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+$('#demo06').animatedModal({
+modalTarget:'modal-06',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+$('#demo07').animatedModal({
+modalTarget:'modal-07',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+$('#demo08').animatedModal({
+modalTarget:'modal-08',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+$('#demo09').animatedModal({
+modalTarget:'modal-09',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
+
+$('#demo10').animatedModal({
+modalTarget:'modal-10',
+animatedIn: 'lightSpeedIn',
+  animatedOut: 'bounceOut',
+  color:'#27ae60'
+               
+            });
